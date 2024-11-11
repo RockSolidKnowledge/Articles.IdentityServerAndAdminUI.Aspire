c26640768ef5c17391528a1d9170e253fc99e0e3
NachoEscrig <nacho.escrig@rocksolidknowledge.com>
2024-11-11 12:34:11 +0000

    Add IdentityServer to Aspire

--- Articles.IdentityServerAndAdminUI.Aspire.AppHost/Program.cs
+++ Articles.IdentityServerAndAdminUI.Aspire.AppHost/Program.cs
@@ -2,4 +2,6 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 builder.AddProject<Projects.Articles_IdentityServerAndAdminUI_Aspire_AdminUI>("articles-identityserverandadminui-aspire-adminui");
 
+builder.AddProject<Projects.Articles_IdentityServerAndAdminUI_Aspire_IdentityServer>("articles-identityserverandadminui-aspire-identityserver");
+
 builder.Build().Run();
